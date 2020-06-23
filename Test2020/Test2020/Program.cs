546cdda82805787c6909392f793ae3a027eba5c5
hellomop <41988525+hellomop@users.noreply.github.com>
2020-06-23 21:42:41 +0800

    测试

---
+++ Test2020/Test2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
